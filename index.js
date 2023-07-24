6dc2ff48e6456a24343bb0a866fc63a004b4cc55
Hadi Jawed <hadijawed132@gmail.com>
2023-07-24 22:53:21 +0500

    .

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 
+var NumOfDrums = document.querySelectorAll(".drum").length;
+for (var i = 0; i < NumOfDrums; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (e) {
+       
+        var buttonInnerHTML=this.innerHTML;
+        console.log(buttonInnerHTML);
+        switch (buttonInnerHTML) {
+            case "w":
+                var tom1 = new Audio('sounds/tom-1.mp3');
+                tom1.play();
+                break;
+            case "a":
+                var tom2 = new Audio('sounds/tom-2.mp3');
+                tom2.play();
+                break;
+            case "s":
+                var tom3= new Audio('sounds/tom-3.mp3');
+                tom3.play();
+                break;
+            case "d":
+                var tom4 = new Audio('sounds/tom-4.mp3');
+                tom4.play();
+                break;
+            case "j":
+                var snare = new Audio('sounds/snare.mp3');
+                snare.play();
+                break;
+            case "k":
+                var kick = new Audio('sounds/kick-bass.mp3');
+                kick.play();
+                break;
+            case "l":
+                var crash = new Audio('sounds/crash.mp3');
+                crash.play();
+                break;
+            default: console.log(InnerHTML);
+        }
 
-for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", handleclick);
+    });
+
 }
 
 
-function handleclick() {
-    alert("i got clicked");
-}
